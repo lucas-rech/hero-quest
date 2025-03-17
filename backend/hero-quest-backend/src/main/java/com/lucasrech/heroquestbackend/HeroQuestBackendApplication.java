be9e3499030fbd83078825b70cad40f159023a6a
Lucas <lucasrech00@gmail.com>
2025-03-17 18:30:54 -0300

    Criado ambiente de desenvolvimento inicial com banco de dados H2

--- backend/hero-quest-backend/src/main/java/com/lucasrech/heroquestbackend/HeroQuestBackendApplication.java
+++ backend/hero-quest-backend/src/main/java/com/lucasrech/heroquestbackend/HeroQuestBackendApplication.java
@@ -1,13 +1,25 @@
 package com.lucasrech.heroquestbackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HeroQuestBackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach((entry) -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(HeroQuestBackendApplication.class, args);
+    }
+
+    @RequestMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World!");
     }
 
 }
