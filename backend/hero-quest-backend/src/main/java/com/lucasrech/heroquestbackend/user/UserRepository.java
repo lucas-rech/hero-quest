ba20770fca8d6676926b39e67623202b5dc7c3f7
Lucas <lucasrech00@gmail.com>
2025-03-18 00:02:55 -0300

    Issue 4: Criada a entidade User e repositório JPA

---
+++ backend/hero-quest-backend/src/main/java/com/lucasrech/heroquestbackend/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.lucasrech.heroquestbackend.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    //TODO: Criar querys customizadas
+}
